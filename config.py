3344dd9ca2337930c9bb1a94aec82e44849a0c24
AquilaUmbra <32918821+AquilaUmbra@users.noreply.github.com>
2018-06-16 23:57:24 -0400

    Config file things
    Make necessary changes to taste etc

---
+++ config.py
@@ -0,0 +1,8 @@
+""" Balderdash Config """
+token = 'paste token here''
+prefix = '// for example'
+description = '''decription'''
+start_msg = 'Logged in as Balderdash'
+
+ann_id = ''
+owner_id = '407010470256443392'
